02aecd186857b04135387682b12a2cecf3e0a693
magatteseye <magatteseye2023@gmail.com>
2026-02-20 23:41:21 +0100

    Mise a jour

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+
+// ✅ Swiper (web components)
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   standalone: true,
@@ -9,5 +13,6 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [CommonModule, RouterLink, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // ✅ important pour swiper-container
 })
 export class HomeComponent {}
